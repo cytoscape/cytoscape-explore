941aaec8e0be01f0ed77ca7594a707816b6698d8
Max Franz <maxkfranz@gmail.com>
2019-05-22 17:19:53 -0400

    Fix name of NetworkEditor class

--- src/client/network-editor.js
+++ src/client/network-editor.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import { CytoscapeSyncher } from '../model/cytoscape-syncher';
 import { EventEmitterProxy } from '../model/event-emitter-proxy';
 
-export class Network extends Component {
+export class NetworkEditor extends Component {
   constructor(props){
     super(props);
 
@@ -75,4 +75,4 @@ export class Network extends Component {
   }
 }
 
-export default Network;
+export default NetworkEditor;
