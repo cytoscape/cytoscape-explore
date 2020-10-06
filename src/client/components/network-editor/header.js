2f8dada6422fb179ef5127eccc89c13b68bf7535
Christian Lopes <chrtannus@gmail.com>
2020-10-06 16:31:30 -0400

    Adds a few disabled account and main-menu items as test/placeholders.

--- src/client/components/network-editor/header.js
+++ src/client/components/network-editor/header.js
@@ -7,20 +7,42 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
+import Popover from "@material-ui/core/Popover";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from '@material-ui/core/MenuItem';
 
 export class Header extends Component {
 
   constructor(props){
     super(props);
     this.busProxy = new EventEmitterProxy(this.props.controller.bus);
+    this.state = {
+      menuName: null,
+      anchorEl: null
+    };
   }
 
   componentWillUnmount(){
     this.busProxy.removeAllListeners();
   }
 
+  handleClick(event, menuName) {
+    this.setState({
+      menuName: menuName,
+      anchorEl: event.currentTarget
+    });
+  }
+
+  handleClose() {
+    this.setState({
+      menuName: null,
+      anchorEl: null
+    });
+  }
+
   render() {
-    // const controller = this.props.controller;
+    const controller = this.props.controller;
+    const { anchorEl, menuName } = this.state;
 
     return (
       <div className="header">
@@ -34,13 +56,32 @@ export class Header extends Component {
             <IconButton edge="start" color="inherit" aria-label="search">
               <SearchIcon />
             </IconButton>
-            <IconButton edge="start" color="inherit" aria-label="account">
+            <IconButton edge="start" color="inherit" aria-label="account" aria-haspopup="true" onClick={e => this.handleClick(e, 'account')}>
               <AccountCircle />
             </IconButton>
-            <IconButton edge="end" color="inherit" aria-label="menu">
+            <IconButton edge="end" color="inherit" aria-label="menu" aria-haspopup="true" onClick={e => this.handleClick(e, 'main')}>
               <MenuIcon />
             </IconButton>
           </Toolbar>
+          <Popover
+            id="menu-popover"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={() => this.handleClose()}
+          >
+            {menuName === 'account' && (
+              <MenuList>
+                <MenuItem disabled={true} onClick={() => this.handleClose()}>NDEx Account</MenuItem>
+                <MenuItem disabled={true} onClick={() => this.handleClose()}>Sign Out</MenuItem>
+              </MenuList>
+            )}
+            {menuName === 'main' && (
+              <MenuList>
+                <MenuItem disabled={true} onClick={() => this.handleClose()}>Layout</MenuItem>
+                <MenuItem disabled={true} onClick={() => this.handleClose()}>Import Network from Cytoscape</MenuItem>
+              </MenuList>
+            )}
+          </Popover>
         </AppBar>
       </div>
     );
