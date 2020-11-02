18647469f1733d8bb33752984b6d6189637b7b7c
Mike Kucera <mikekucera@gmail.com>
2020-11-02 10:48:38 -0500

    Use material-ui for style picker

--- src/client/components/style/style-picker-button.js
+++ src/client/components/style/style-picker-button.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Tippy from '@tippy.js/react';
+import React, {Component} from 'react';
 import StylePicker from '../style/style-picker';
+import Popover from '@material-ui/core/Popover';
 import PropTypes from 'prop-types';
 
 /**
@@ -9,25 +9,52 @@ import PropTypes from 'prop-types';
  * @param {String} props.buttonIcon The CSS icon class of the icon in the button
  * @param {String} props.title Title of the button (shown in tooltip)
  */
-export function StylePickerButton(props)  {
-  const ref = React.createRef();
-  const tooltip = props.title;
-  return (
-    <Tippy
-      interactive={true}
-      trigger='click'
-      theme='light'
-      onShow={() => ref.current.onShow && ref.current.onShow()}
-      content={
-        <StylePicker ref={ref} {...props} />
-      }>
-      <Tippy content={tooltip}>
-        <button className="style-panel-button plain-button">
-          <i className="material-icons">{props.buttonIcon}</i>
+export class StylePickerButton extends Component  {
+
+  constructor(props) {
+    super(props);
+    this.state = { anchorEl: null };
+  }
+
+  handleClick(event) {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose() {
+    this.setState({ anchorEl: null });
+  }
+
+  render() {
+    const ref = React.createRef();
+    // const tooltip = this.props.title;
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <button 
+          onClick={e => this.handleClick(e)}
+          className="style-panel-button plain-button">
+          <i className="material-icons">{this.props.buttonIcon}</i>
         </button>
-      </Tippy>
-    </Tippy>
-  );
+        <Popover 
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={() => this.handleClose()}
+          onEnter={() => ref.current.onShow && ref.current.onShow()}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <StylePicker ref={ref} {...this.props} />
+        </Popover>
+      </div>
+    );
+  }
 }
 
 StylePickerButton.propTypes = {
