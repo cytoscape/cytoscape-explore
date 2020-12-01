1ca8b62ef2a32018f449da0915dca14856d84e00
Max Franz <maxkfranz@gmail.com>
2020-12-01 16:41:12 -0500

    Use Open Sans in Material UI

--- src/client/theme.js
+++ src/client/theme.js
@@ -6,14 +6,22 @@ const theme = createMuiTheme({
       main: '#1D6995',
       light: '#5C95B7',
       dark: '#053E60',
-      contrastText: '#fff',
+      contrastText: '#fff'
     },
     secondary: {
       main: '#1D6995',
       light: '#5C95B7',
       dark: '#053E60',
-      contrastText: '#fff',
-    },
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    fontFamily: 'Open Sans, Helvetica Neue, Helvetica, sans-serif'
+  },
+  props: {
+    MuiButtonBase: {
+      // disableRipple: true // No more ripple, on the whole application
+    }
   },
 });
 
