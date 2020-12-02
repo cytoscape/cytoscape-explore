c72066b22c3ce8b53c70b7d96a0838753f1aafc4
Christian Lopes <chrtannus@gmail.com>
2020-12-02 12:17:48 -0500

    Fixes lint error: 'classNames' is defined but never used.

--- src/client/components/network-editor/tool-panel.js
+++ src/client/components/network-editor/tool-panel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
 import EventEmitterProxy from '../../../model/event-emitter-proxy';
 import PropTypes from 'prop-types';
 import { NetworkEditorController } from './controller';
