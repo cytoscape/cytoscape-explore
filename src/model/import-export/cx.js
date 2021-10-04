8e572ac9c97ff7eec97d105e4bbd5f3827732179
Jing Chen <jic002@ucsd.edu>
2021-10-04 16:30:38 -0700

    CX import function in progress.

--- src/model/import-export/cx.js
+++ src/model/import-export/cx.js
@@ -2,42 +2,6 @@ import { convertCX } from './cx/cx-converter.js';
 import { convertCY } from './cx/cy-converter.js';
 import { styleFactory } from '../../model/style';
 
-const DEFAULT_STYLE_FACTORY_FUNCTIONS = {
-  'NODE_BACKGROUND_COLOR': (value) => styleFactory.color(value),
-  'NODE_WIDTH': (value) => styleFactory.number(value),
-  'NODE_HEIGHT': (value) => styleFactory.number(value),
-  'NODE_LABEL': (value) => styleFactory.string(value),
-  'NODE_BORDER_COLOR': (value) => styleFactory.color(value),
-  'NODE_BORDER_WIDTH': (value) => styleFactory.number(value),
-  'NODE_SHAPE': (value) => styleFactory.string(value),
-
-  'EDGE_WIDTH': (value) => styleFactory.number(value),
-  'EDGE_LINE_COLOR': (value) => styleFactory.color(value),
-  'EDGE_SOURCE_ARROW_COLOR': (value) => styleFactory.color(value),
-  'EDGE_SOURCE_ARROW_SHAPE': (value) => styleFactory.string(value),
-  'EDGE_TARGET_ARROW_COLOR': (value) => styleFactory.color(value),
-  'EDGE_TARGET_ARROW_SHAPE': (value) => styleFactory.string(value),
-  'EDGE_LINE_STYLE': (value) => styleFactory.string(value)
-};
-
-const DEFAULT_JS_STYLE_NAMES = {
-  'NODE_BACKGROUND_COLOR': 'background-color',
-  'NODE_WIDTH': 'width',
-  'NODE_HEIGHT': 'height',
-  'NODE_LABEL': 'label',
-  'NODE_BORDER_COLOR': 'border-color',
-  'NODE_BORDER_WIDTH': 'border-width',
-  'NODE_SHAPE': 'shape',
-
-  'EDGE_WIDTH': 'width',
-  'EDGE_LINE_COLOR': 'line-color',
-  'EDGE_SOURCE_ARROW_COLOR': 'source-arrow-color',
-  'EDGE_SOURCE_ARROW_SHAPE': 'source-arrow-shape',
-  'EDGE_TARGET_ARROW_COLOR': 'target-arrow-color',
-  'EDGE_TARGET_ARROW_SHAPE': 'target-arrow-shape',
-  'EDGE_LINE_STYLE': 'line-style'
-};
-
 const colorMapper = {
   valueCvtr: ((value) => styleFactory.color(value)),
   jsValueType: styleFactory.color,
@@ -64,53 +28,64 @@ const STYLE_CONVERTING_TABLE = {
       { jsVPName :  'background-color',
         mapper: colorMapper
       },
-  'NODE_WIDTH': {jsVPName :  'width',
-          mapper: numberMapper
-       },
-  'NODE_HEIGHT': {jsVPName :  'height',
-    mapper: numberMapper
-  },
-  'NODE_LABEL': {jsVPName :  'label',
-          mapper: stringMapper
-        },
-  'NODE_BORDER_COLOR': {jsVPName :  'border-color',
-          mapper: colorMapper
-        },
-  'NODE_BORDER_WIDTH': {jsVPName :  'border-width',
-    mapper: numberMapper
-  },
-  'NODE_SHAPE':{jsVPName :  'shape',
-    mapper: stringMapper
-  },
+  'NODE_WIDTH':
+      { jsVPName :  'width',
+        mapper: numberMapper
+      },
+  'NODE_HEIGHT':
+      { jsVPName :  'height',
+        mapper: numberMapper
+      },
+  'NODE_LABEL':
+      { jsVPName :  'label',
+        mapper: stringMapper
+      },
+  'NODE_BORDER_COLOR':
+      { jsVPName :  'border-color',
+        mapper: colorMapper
+      },
+  'NODE_BORDER_WIDTH':
+      { jsVPName :  'border-width',
+        mapper: numberMapper
+      },
+  'NODE_SHAPE':
+      { jsVPName :  'shape',
+        mapper: stringMapper
+      },
 
-  'EDGE_WIDTH': {jsVPName :  'width',
-    mapper: numberMapper
-  },
-  'EDGE_LINE_COLOR': { jsVPName :  'line-color',
-    mapper: colorMapper
-  },
-  'EDGE_SOURCE_ARROW_COLOR':{ jsVPName :  'source-arrow-color',
-    mapper: colorMapper
-  },
-  'EDGE_SOURCE_ARROW_SHAPE': {jsVPName :  'source-arrow-shape',
-    mapper: stringMapper
-  },
-  'EDGE_TARGET_ARROW_COLOR':{ jsVPName :  'target-arrow-color',
-    mapper: colorMapper
-  },
-  'EDGE_TARGET_ARROW_SHAPE': {jsVPName :  'target-arrow-shape',
-    mapper: stringMapper
-  },
-  'EDGE_LINE_STYLE': {jsVPName :  'line-style',
-    mapper: stringMapper
-  }
+  'EDGE_WIDTH':
+      { jsVPName :  'width',
+        mapper: numberMapper
+      },
+  'EDGE_LINE_COLOR':
+      { jsVPName :  'line-color',
+        mapper: colorMapper
+      },
+  'EDGE_SOURCE_ARROW_COLOR':
+      { jsVPName :  'source-arrow-color',
+        mapper: colorMapper
+      },
+  'EDGE_SOURCE_ARROW_SHAPE':
+      { jsVPName :  'source-arrow-shape',
+        mapper: stringMapper
+      },
+  'EDGE_TARGET_ARROW_COLOR':
+      { jsVPName :  'target-arrow-color',
+        mapper: colorMapper
+      },
+  'EDGE_TARGET_ARROW_SHAPE':
+      { jsVPName :  'target-arrow-shape',
+        mapper: stringMapper
+      },
+  'EDGE_LINE_STYLE':
+      { jsVPName :  'line-style',
+        mapper: stringMapper
+      }
 };
 
-
-
 const convertStyle = (visualPropertyKey, cxValue) => {
-  if (DEFAULT_STYLE_FACTORY_FUNCTIONS[visualPropertyKey]) {
-    return DEFAULT_STYLE_FACTORY_FUNCTIONS[visualPropertyKey](cxValue);
+  if (STYLE_CONVERTING_TABLE[visualPropertyKey]) {
+    return STYLE_CONVERTING_TABLE[visualPropertyKey].mapper.valueCvtr(cxValue);
   } else {
     console.warn(`Visual Property ${visualPropertyKey} cannot be resolved to styleFactory function.`);
   }
@@ -120,7 +95,8 @@ const applyDefaultPropertyMap = (vizmapper, defaultProperties) => {
   Object.keys(defaultProperties).forEach(visualPropertyKey => {
 
     const visualPropertyValue = defaultProperties[visualPropertyKey];
-    const vizmapperPropertyKey = DEFAULT_JS_STYLE_NAMES[visualPropertyKey];
+    const vizmapperPropertyKey = STYLE_CONVERTING_TABLE[visualPropertyKey] != undefined ?
+                    STYLE_CONVERTING_TABLE[visualPropertyKey].jsVPName : undefined;
     const vizmapperPropertyValue = convertStyle(visualPropertyKey, visualPropertyValue);
 
     if (!vizmapperPropertyKey) {
@@ -162,32 +138,35 @@ const _continuousMappingCvtr = (result, currentV) => {
  */
 const convertMapping = (selector, vizmapper, styleMappings, defaultTable ) =>   {
   for (const [vpName, mapping] of Object.entries(styleMappings)) {
-    const jsvpName = DEFAULT_JS_STYLE_NAMES[vpName];
-    if ( jsvpName ) {
-      const attr = mapping.definition.attribute;
-      if ( mapping.type ==="DISCRETE") {
-        const valueMap = {};
-        mapping.definition.map.forEach (function (mappingEntry)  {
-          const newValue = STYLE_CONVERTING_TABLE[vpName].mapper.valueCvtr(mappingEntry.vp).value;
-          valueMap[mappingEntry.v] =  newValue;
-        });
-        const defaultValue = STYLE_CONVERTING_TABLE[vpName].mapper.valueCvtr(defaultTable[vpName]).value;
-        const style = STYLE_CONVERTING_TABLE[vpName].mapper.discreteMappingFactory(attr,defaultValue, valueMap);
-        vizmapper.set(selector,STYLE_CONVERTING_TABLE[vpName].jsVPName, style);
-      } else if (mapping.type === 'CONTINUOUS') {
+    if (STYLE_CONVERTING_TABLE[vpName]) {
+        const jsvpName = STYLE_CONVERTING_TABLE[vpName].jsVPName;
+        if (jsvpName) {
+            const attr = mapping.definition.attribute;
+            if (mapping.type === "DISCRETE") {
+                const valueMap = {};
+                mapping.definition.map.forEach(function (mappingEntry) {
+                    const newValue = STYLE_CONVERTING_TABLE[vpName].mapper.valueCvtr(mappingEntry.vp).value;
+                    valueMap[mappingEntry.v] = newValue;
+                });
+                const defaultValue = STYLE_CONVERTING_TABLE[vpName].mapper.valueCvtr(defaultTable[vpName]).value;
+                const style = STYLE_CONVERTING_TABLE[vpName].mapper.discreteMappingFactory(attr, defaultValue, valueMap);
+                vizmapper.set(selector, STYLE_CONVERTING_TABLE[vpName].jsVPName, style);
+            } else if (mapping.type === 'CONTINUOUS') {
 
-        let newList = mapping.definition.map.reduce(_continuousMappingCvtr, {dataValues:[], styleValues:[]});
-        let cyStyleValues = newList.styleValues.map( x => STYLE_CONVERTING_TABLE[vpName].mapper.valueCvtr(x).value );
-    /*    const segmentCnt = mapping.definition.map.length;
-        const lowerDef = mapping.definition.map[1];
-        const upperDef = mapping.definition.map[segmentCnt-2]; */
-        const style = STYLE_CONVERTING_TABLE[vpName].mapper.cotinuousMappingFactory(attr,
-             newList.dataValues, cyStyleValues);
-        vizmapper.set(selector, STYLE_CONVERTING_TABLE[vpName].jsVPName, style);
-      } else if ( mapping.type === 'PASSTHROUGH') {
-        vizmapper.set(selector,STYLE_CONVERTING_TABLE[vpName].jsVPName,
-            STYLE_CONVERTING_TABLE[vpName].mapper.passthroughMappingFactory(attr));
-      }
+                let newList = mapping.definition.map.reduce(_continuousMappingCvtr, {dataValues: [], styleValues: []});
+                let cyStyleValues = newList.styleValues.map(x => STYLE_CONVERTING_TABLE[vpName].mapper.valueCvtr(x).value);
+                const style = STYLE_CONVERTING_TABLE[vpName].mapper.cotinuousMappingFactory(attr,
+                    newList.dataValues, cyStyleValues);
+                vizmapper.set(selector, STYLE_CONVERTING_TABLE[vpName].jsVPName, style);
+            } else if (mapping.type === 'PASSTHROUGH') {
+                if ( STYLE_CONVERTING_TABLE[vpName].mapper.passthroughMappingFactory ) {
+                    vizmapper.set(selector, STYLE_CONVERTING_TABLE[vpName].jsVPName,
+                        STYLE_CONVERTING_TABLE[vpName].mapper.passthroughMappingFactory(attr));
+                } else {
+                    console.warn(`PassthroughMapping is not supported on  ${vpName}.`);
+                }
+            }
+        }
     }
   }
 };
