f9ba077e9b1d18559ca17a4846cc856b4bfc3850
d2fong <d2fong@uwaterloo.ca>
2021-10-13 16:44:07 -0400

    remove useless test

--- test/cx-conversion-test.js
+++ test/cx-conversion-test.js
@@ -1,17 +1,5 @@
 import { assert, expect } from 'chai';
-import { isSavedAspect } from '../src/model/import-export/cx/cx-converter'
 
 describe('CX Conversion', () => {
-  
-
-  beforeEach('before each', () => {
-   
-  });
-
-
-  it('saved aspects', () => {
-    expect(isSavedAspect({'metaData': 'some stuff'})).to.be.true
-  });
-
 
 });
