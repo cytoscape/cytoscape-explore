d7483ac21d8f444da3e3944ee6cf03eaedaa53e6
d2fong <d2fong@uwaterloo.ca>
2021-10-14 13:40:42 -0400

    delete unnecessary code

--- src/server/index.js
+++ src/server/index.js
@@ -61,12 +61,7 @@ app.use(express.static(path.join(__dirname, '../..', 'public')));
 
 // proxy requests under /db to the CouchDB server
 app.use('/db', secrets); // apply security before proxy
-app.use('/db', proxy(COUCHDB_URL /*,{
-  proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
-    proxyReqOpts.auth = 'admin:admin';
-    return proxyReqOpts;
-  }}*/
-));
+app.use('/db', proxy(COUCHDB_URL));
 
 app.use('/api', apiRouter);
 app.use('/', indexRouter);
--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,10 +1,8 @@
 import Express from 'express';
 import documentRouter from './document';
-import ndexRouter from './ndex';
 
 const http = Express.Router();
 
 http.use('/document', documentRouter);
-http.use('/ndex', ndexRouter);
 
 export default http;
