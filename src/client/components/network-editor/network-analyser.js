0866ea9ee18602bd5139282ac1eaf7ddcead3268
Christian Lopes <chrtannus@gmail.com>
2021-10-26 17:12:59 -0400

    Fixes warning "Property 'range' does not exist on type(...)".

--- src/client/components/network-editor/network-analyser.js
+++ src/client/components/network-editor/network-analyser.js
@@ -153,7 +153,8 @@ export class NetworkAnalyser {
             [ ATTR_TYPE.UNKNOWN, { set: new Set() } ],
             [ ATTR_TYPE.MISSING, { set: first ? new Set() : COMMON } ],  // if we have processed at least one node but haven't seen this attribute yet then make unknown the common type
             [ type,              { set: first ? COMMON : new Set(ele) } ]  // override previous entry for the type
-          ])
+          ]),
+          range: null,
         };
         newInfo.range = new Range(newInfo);
         if(type == ATTR_TYPE.NUMBER) {
