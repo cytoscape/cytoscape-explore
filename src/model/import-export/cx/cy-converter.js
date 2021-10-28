a4b061e2bffa1abc67276db3474c36624ed23449
d2fong <d2fong@uwaterloo.ca>
2021-10-28 15:05:48 -0400

    WIP rework CX export

--- src/model/import-export/cx/cy-converter.js
+++ src/model/import-export/cx/cy-converter.js
@@ -2,7 +2,119 @@
  import { rgbObjToHex, MAPPING, STYLE_TYPE, DEFAULT_NODE_STYLE, DEFAULT_EDGE_STYLE } from '../../style';
  import _ from 'lodash';
 
- // current CX node properties supported
+
+ const cxStyleConverter = ({type, value, cxVPName, cxParentVPName}) => {
+  return type === STYLE_TYPE.COLOR ? rgbObjToHex(value) : value;
+ };
+
+ const cxStyleMutationReducer = (curStyle, nextStyle) => nextStyle;
+
+ const cxStyleOverrideReducer = (curStyle, nextStyle) => Object.assign({}, curStyle, nextStyle);
+
+ const CE_2_CX_STYLE_CONVERTING_TABLE = {
+   node: {
+    'shape': {
+      cxVPName: 'NODE_SHAPE',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'border-width': {
+      cxVPName: 'NODE_BORDER_WIDTH',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'border-color': {
+      cxVPName: 'NODE_BORDER_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'label': {
+      cxVPName: 'NODE_LABEL',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'height': {
+      cxVPName: 'NODE_HEIGHT',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'width': {
+      cxVPName: 'NODE_WIDTH',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'background-color': {
+      cxVPName: 'NODE_BACKGROUND_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'text-halign': {
+      cxVPName: 'HORIZONTAL_ALIGN',
+      cxParentVPName: 'NODE_LABEL_POSITION',
+      converter: cxStyleConverter,
+      reducer: cxStyleOverrideReducer
+    },
+    'text-valign': {
+      cxVPName: 'VERTICAL_ALIGN',
+      cxParentVPName: 'NODE_LABEL_POSITION',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'color': {
+      cxVPName: 'NODE_LABEL_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'font-size': {
+      cxVPName: 'NODE_LABEL_FONT_SIZE',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+   },
+   edge: {
+    'line-style': {
+      cxVPName: 'EDGE_LINE_STYLE',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'target-arrow-color': {
+      cxVPName: 'EDGE_TARGET_ARROW_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'target-arrow-shape': {
+      cxVPName: 'EDGE_TARGET_ARROW_SHAPE',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'source-arrow-color': {
+      cxVPName: 'EDGE_SOURCE_ARROW_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'source-arrow-shape': {
+      cxVPName: 'EDGE_SOURCE_ARROW_SHAPE',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'color': {
+      cxVPName: 'EDGE_LABEL_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'width': {
+      cxVPName: 'EDGE_LINE_WIDTH',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+    'line-color': {
+      cxVPName: 'EDGE_LINE_COLOR',
+      converter: cxStyleConverter,
+      reducer: cxStyleMutationReducer
+    },
+   }
+ };
+
  const cy2cxNodeVisualProp = {
    // cy.js to cx
    'shape': 'NODE_SHAPE',
@@ -87,115 +199,149 @@
    return null;
  };
 
- /**
-  * Export a Cytoscape instance to CX2 format
-  * @param {Cytoscape.Core} cy
-  */
- export const convertCY = (cy) => {
-   const cx2Descriptor = {
-     CXVersion: "2.0",
-     hasFragments: false
-   };
+ export const cx2Descriptor = () => ({
+    CXVersion: "2.0",
+    hasFragments: false
+ });
 
-   const nodeAttributes = {
-     cytoscapeExploreId: {
-       d: 'string'
-     }
-   };
-   const edgeAttributes = {
-     cytoscapeExploreId: {
-       d: 'string'
-     }
-   };
-   const cxNodes = [];
-   const cxEdges = [];
+ export const cxDataAspects = (cy) => {
+  const nodeAttributes = {
+    cytoscapeExploreId: {
+      d: 'string'
+    }
+  };
 
-   cy.elements().forEach((ele, index) => {
-     let v = {};
-     let isNode = ele.isNode();
-     ele.scratch('_exportCX2Id', index);
+  const edgeAttributes = {
+    cytoscapeExploreId: {
+      d: 'string'
+    }
+  };
 
-     Object.keys(ele.data()).forEach(key => {
-       let value = ele.data(key);
+  const cxNodes = [];
+  const cxEdges = [];
 
-       if(key !== 'id' && key !== 'source' && key !== 'target') {
-         let type = getCXType(value);
+  cy.elements().forEach((ele, index) => {
+    let v = {};
+    let isNode = ele.isNode();
+    ele.scratch('_exportCX2Id', index);
 
-         // TODO also need to check that the type for each attribute is the same for
-         // each node
-         if(type != null){
-           v[key] = value;
-           let attributeDict = isNode ? nodeAttributes : edgeAttributes;
+    Object.keys(ele.data()).forEach(key => {
+      let value = ele.data(key);
 
-           attributeDict[key] = {
-             d: type
-           };
-         }
-       }
-     });
+      if(key !== 'id' && key !== 'source' && key !== 'target') {
+        let type = getCXType(value);
 
-     if(isNode){
-       cxNodes.push({
-         id: index,
-         x: ele.position('x'),
-         y: ele.position('y'),
-         v
-       });
-     } else {
-       cxEdges.push({
-         id: index,
-         s: ele.source().scratch('_exportCX2Id'),
-         t: ele.target().scratch('_exportCX2Id'),
-         v
-       });
-     }
-   });
+        // TODO also need to check that the type for each attribute is the same for
+        // each node
+        if(type != null){
+          v[key] = value;
+          let attributeDict = isNode ? nodeAttributes : edgeAttributes;
 
-   const attributeDeclarationsAspect = {
-     attributeDeclarations: [{
-       networkAttributes: {
-         name: { d: 'string', v: ''},
-         description: { d: 'string', v: ''},
-         version: { d: 'string', v: ''},
-         cytoscapeExploreId: { d: 'string', v: ''}
-       },
-       nodes: nodeAttributes,
-       edges: edgeAttributes
-     }]
-   };
+          attributeDict[key] = {
+            d: type
+          };
+        }
+      }
+    });
 
-   const networkAttributesAspect = {
-     networkAttributes: [{
-       name: cy.data('name') || 'cyexplore',
-       description: cy.data('description') || 'description',
-       version: cy.data('version') || '1.0'
-     }]
-   };
+    if(isNode){
+      cxNodes.push({
+        id: index,
+        x: ele.position('x'),
+        y: ele.position('y'),
+        v
+      });
+    } else {
+      cxEdges.push({
+        id: index,
+        s: ele.source().scratch('_exportCX2Id'),
+        t: ele.target().scratch('_exportCX2Id'),
+        v
+      });
+    }
+  });
 
-   const nodesAspect = { nodes: cxNodes };
-   const edgesAspect = { edges: cxEdges };
+  const attributeDeclarationsAspect = {
+    attributeDeclarations: [{
+      networkAttributes: {
+        name: { d: 'string', v: ''},
+        description: { d: 'string', v: ''},
+        version: { d: 'string', v: ''},
+        cytoscapeExploreId: { d: 'string', v: ''}
+      },
+      nodes: nodeAttributes,
+      edges: edgeAttributes
+    }]
+  };
 
-   const visualPropertiesAspect = {
-     visualProperties: [
-       {
-         default: {
-           network: {
-             NETWORK_BACKGROUND_COLOR: "#FFFFFF"
-           },
-           node: {},
-           edge: {}
-         },
-         nodeMapping: {},
-         edgeMapping: {}
-       }
-     ]
-   };
+  const networkAttributesAspect = {
+    networkAttributes: [{
+      name: cy.data('name') || 'cyexplore',
+      description: cy.data('description') || 'description',
+      version: cy.data('version') || '1.0'
+    }]
+  };
 
-   const nodeBypassesAspect = { nodeBypasses: [] };
-   const edgeBypassesAspect = { edgeBypasses: [] };
+  const nodesAspect = { nodes: cxNodes };
+  const edgesAspect = { edges: cxEdges };
 
-   const styleSnapshot = _.cloneDeep(cy.data('_styles') || {});
-   const bypassSnapshot = _.cloneDeep(cy.data('_bypasses') || {});
+  return {
+    nodesAspect,
+    edgesAspect,
+    attributeDeclarationsAspect,
+    networkAttributesAspect
+  };
+ };
+
+ export const cxVisualPropertiesAspects = cy => {
+  const visualPropertiesAspect = {
+    visualProperties: [
+      {
+        default: {
+          network: {
+            NETWORK_BACKGROUND_COLOR: "#FFFFFF"
+          },
+          node: {},
+          edge: {}
+        },
+        nodeMapping: {},
+        edgeMapping: {}
+      }
+    ]
+  };
+
+  const nodeBypassesAspect = { nodeBypasses: [] };
+  const edgeBypassesAspect = { edgeBypasses: [] };
+
+  const styleSnapshot = _.cloneDeep(cy.data('_styles') || {});
+  const bypassSnapshot = _.cloneDeep(cy.data('_bypasses') || {});
+
+
+  const defaultVisualProperties = [
+    ...Object.entries(DEFAULT_NODE_STYLE).map(([cyjsVPName, cyjsVPValue]) => {
+     return {
+       group: 'node',
+       cyjsVPName,
+       cyjsVPValue
+     };
+    }),
+    ...Object.entries(DEFAULT_EDGE_STYLE).map(([cyjsVPName, cyjsVPValue]) => {
+     return {
+       group: 'edge',
+       cyjsVPName,
+       cyjsVPValue
+     };
+    })
+  ];
+
+  // defaultVisualProperties.forEach(({group, cyjsVPName, cyjsVPValue}) => {
+  //  let { type, value, mapping } = cyjsVPValue;
+  //  let {cxVPName, cxParentVPName, converter } = CE_2_CX_STYLE_CONVERTING_TABLE[group][cyjsVPName];
+
+  //  // mappings go in the nodeMapping/edgeMapping object
+  //  if(cyjsVPName === 'label' || mapping !== MAPPING.VALUE){
+  //    return;
+  //  }
 
    const validDefaultNodeStyles = _.cloneDeep(DEFAULT_NODE_STYLE);
    const validDefaultEdgeStyles = _.cloneDeep(DEFAULT_EDGE_STYLE);
@@ -207,6 +353,10 @@
    Object.keys(validDefaultEdgeStyles)
    .filter(key => cy2cxEdgeVisualProp[key] == null)
    .forEach(key => delete validDefaultEdgeStyles[key]);
+
+     //  visualPropertiesAspect.visualProperties[0].default[group] = converter(Object.assign({}, cyjsVPValue, cxVPName, cxParentVPName));
+  // });
+
 
    // 1. populate defaults with the default styles from style.js
    // 2. iterate over each entry in the style snapshot
@@ -275,21 +425,35 @@
            };
            break;
          case MAPPING.DISCRETE: {
-           // discrete mappings specify colors as { r, g, b } objects
-           // turn them into strings before updating them in the cx output
-           defaultAspect[cxStyleName] = type === STYLE_TYPE.COLOR ? rgbObjToHex(defaultValue) : defaultValue;
-           mappingAspect[cxStyleName] = {
-             type: mapping,
-             definition: {
-               attribute: data,
-               map: Object.entries(styleValues).map(([attrClass, attrStyleValue]) => {
-                 return {
-                   v: attrClass,
-                   vp: type === STYLE_TYPE.COLOR ? rgbObjToHex(attrStyleValue) : attrStyleValue
-                 };
-               })
-             }
-           };
+          if(cyStyleName === 'text-valign' || cyStyleName === 'text-halign'){
+            defaultAspect[cxStyleName] = cxStyleConverter({type, value: defaultValue});
+            mappingAspect[cxStyleName] = {
+               type: mapping,
+               definition: {
+                 attribute: data,
+                 map: Object.entries(styleValues).map(([attrClass, attrStyleValue]) => {
+                   return {
+                     v: attrClass,
+                     vp: cxStyleConverter({ type, value: attrStyleValue })
+                   };
+                 })
+               }
+             };
+          } else {
+            defaultAspect[cxStyleName] = cxStyleConverter({type, value: defaultValue});
+            mappingAspect[cxStyleName] = {
+               type: mapping,
+               definition: {
+                 attribute: data,
+                 map: Object.entries(styleValues).map(([attrClass, attrStyleValue]) => {
+                   return {
+                     v: attrClass,
+                     vp: cxStyleConverter({ type, value: attrStyleValue })
+                   };
+                 })
+               }
+             };
+          }
            break;
          }
          case MAPPING.LINEAR: {
@@ -344,72 +508,98 @@
      }
    });
 
-   const visualEditorPropertiesAspect = {
-     visualEditorProperties: [
-       {
-         properties: {
-           nodeSizeLocked: false
-         }
-       }
-     ]
+   return {
+    visualPropertiesAspect,
+    nodeBypassesAspect,
+    edgeBypassesAspect
    };
+ };
 
-   const statusAspect = {
-     status: [{
-       error: '',
-       success: true
-     }]
-   };
-   const metadataAspect = {
-     metaData: [
-       {
-         elementCount: attributeDeclarationsAspect.attributeDeclarations.length,
-         name: 'attributeDeclarations'
-       },
-       {
-         elementCount: networkAttributesAspect.networkAttributes.length,
-         name: 'networkAttributes'
-       },
-       {
-         elementCount: nodesAspect.nodes.length,
-         name: 'nodes'
-       },
-       {
-         elementCount: edgesAspect.edges.length,
-         name: 'edges'
-       },
-       {
-         elementCount: visualPropertiesAspect.visualProperties.length,
-         name: 'visualProperties'
-       },
-       {
-         elementCount: nodeBypassesAspect.nodeBypasses.length,
-         name: 'nodeBypasses'
-       },
-       {
-         elementCount: edgeBypassesAspect.edgeBypasses.length,
-         name: 'edgeBypasses'
-       },
-       {
-         elementCount: visualEditorPropertiesAspect.visualEditorProperties.length,
-         name: 'visualEditorProperties'
-       }
-     ]
-   };
+ /**
+  * Export a Cytoscape instance to CX2 format
+  * @param {Cytoscape.Core} cy
+  */
+ export const convertCY = (cy) => {
 
-   const cx2Output = [
-     cx2Descriptor,
-     metadataAspect,
-     attributeDeclarationsAspect,
-     networkAttributesAspect,
-     nodesAspect,
-     edgesAspect,
-     visualPropertiesAspect,
-     nodeBypassesAspect,
-     edgeBypassesAspect,
-     visualEditorPropertiesAspect,
-     statusAspect,
-   ];
+  const {
+    nodesAspect,
+    edgesAspect,
+    attributeDeclarationsAspect,
+    networkAttributesAspect
+  } = cxDataAspects(cy);
 
-   return cx2Output;
+  const {
+    visualPropertiesAspect,
+    nodeBypassesAspect,
+    edgeBypassesAspect
+  } = cxVisualPropertiesAspects(cy);
+
+  const visualEditorPropertiesAspect = {
+    visualEditorProperties: [
+      {
+        properties: {
+          nodeSizeLocked: false
+        }
+      }
+    ]
+  };
+
+  const statusAspect = {
+    status: [{
+      error: '',
+      success: true
+    }]
+  };
+  const metadataAspect = {
+    metaData: [
+      {
+        elementCount: attributeDeclarationsAspect.attributeDeclarations.length,
+        name: 'attributeDeclarations'
+      },
+      {
+        elementCount: networkAttributesAspect.networkAttributes.length,
+        name: 'networkAttributes'
+      },
+      {
+        elementCount: nodesAspect.nodes.length,
+        name: 'nodes'
+      },
+      {
+        elementCount: edgesAspect.edges.length,
+        name: 'edges'
+      },
+      {
+        elementCount: visualPropertiesAspect.visualProperties.length,
+        name: 'visualProperties'
+      },
+      {
+        elementCount: nodeBypassesAspect.nodeBypasses.length,
+        name: 'nodeBypasses'
+      },
+      {
+        elementCount: edgeBypassesAspect.edgeBypasses.length,
+        name: 'edgeBypasses'
+      },
+      {
+        elementCount: visualEditorPropertiesAspect.visualEditorProperties.length,
+        name: 'visualEditorProperties'
+      }
+    ]
+  };
+
+  const cx2Output = [
+    cx2Descriptor(),
+    metadataAspect,
+    attributeDeclarationsAspect,
+    networkAttributesAspect,
+    nodesAspect,
+    edgesAspect,
+    visualPropertiesAspect,
+    nodeBypassesAspect,
+    edgeBypassesAspect,
+    visualEditorPropertiesAspect,
+    statusAspect,
+  ];
+
+  return cx2Output;
  };
