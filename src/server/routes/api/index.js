15356401c617d73de40ce6c742510344f49c81d7
Mike Kucera <mikekucera@gmail.com>
2022-01-26 13:46:46 -0500

    Start of thumbnail API.

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,10 +1,12 @@
 import Express from 'express';
 import documentRouter from './document';
 import historyRouter from './history';
+import thumbnailRouter from './thumbnail';
 
 const http = Express.Router();
 
 http.use('/document', documentRouter);
 http.use('/history', historyRouter);
+http.use('/thumbnail', thumbnailRouter);
 
 export default http;
---
+++ src/server/routes/api/thumbnail.js
@@ -0,0 +1,108 @@
+import Express from 'express';
+import PouchDB  from 'pouchdb';
+import _ from 'lodash';
+import { COUCHDB_PASSWORD, COUCHDB_URL, COUCHDB_USER, USE_COUCH_AUTH } from '../../env';
+
+
+// TODO Rewrite this to use 'import'
+const cytosnap = require('cytosnap');
+const snap = cytosnap();
+
+async function createThumbnail(id) {
+  await snap.start();
+  console.log("Cytosnap stared!");
+
+  const db = createPouchInstance(id);
+  console.log("PouchDB instance created!");
+
+  const elements = await createCytoscapeElementsJSON(db, id);
+
+  var options = {
+    // cytoscape.js options
+    elements, // cytoscape.js elements json
+    //style: undefined, // a cytoscape.js stylesheet in json format (or a function that returns it)
+    //layout: undefined, // a cytoscape.js layout options object (or a function that returns it)
+    // (specifying style or layout via a function is useful in cases where you can't send properly serialisable json)
+   
+    // image export options
+    resolvesTo: 'base64', // output, one of 'base64uri' (default), 'base64', 'stream', or 'json' (export resultant node positions from layout)
+    format: 'png', // 'png' or 'jpg'/'jpeg' (n/a if resolvesTo: 'json')
+    quality: 85, // quality of image if exporting jpg format, 0 (low) to 100 (high)
+    background: 'transparent', // a css colour for the background (transparent by default)
+    width: 200, // the width of the image in pixels
+    height: 200 // the height of the image in pixels
+  };
+  
+  // TODO figure out how to use an image stream, shouldn't have to encode/decode base64
+  const image = await snap.shot(options);
+  console.log("Image generated! " + image);
+  return image;
+}
+
+
+async function createCytoscapeElementsJSON(db, id) {
+  const docs = await db.allDocs({
+    include_docs: true
+  });
+  console.log("Docs retreived!");
+
+  if(docs.total_rows === 0) {
+    new Error(`The database is empty: ${id}`);
+  }
+
+  let eleJsons = [];
+  let styleJson = []; // TODO ???
+
+  for(let i = 0; i < docs.rows.length; i++) {
+    let row = docs.rows[i];
+    let { doc } = row;
+
+    if(row.id === id){
+      // TODO get the style
+    } else if(row.id !== 'snapshots') { // TODO Remove the check for 'snapshots'
+      eleJsons.push({
+        data: _.clone(doc.data),
+        position: _.clone(doc.position),
+      });
+    }
+  }
+
+  return eleJsons;
+}
+
+
+// TODO When/if the database format changes to store the entire network in a single 
+// document, then we can just use fetch and the couch http API to get the document. 
+// For now using Pouch is more convenient because it automatically starts a replication 
+// that pulls in a consistent snapshot of the network at the time the replication starts.
+function createPouchInstance(id) {
+  let options = {};
+  if (USE_COUCH_AUTH) {
+    options.auth = {
+      username: COUCHDB_USER,
+      password: COUCHDB_PASSWORD
+    };
+  }
+  return new PouchDB(`${COUCHDB_URL}/${id}`, options);
+}
+
+
+
+const http = Express.Router();
+
+http.get('/:id', async function(req, res, next) {
+  try {
+    const { id } = req.params;
+    const imageEncoded = await createThumbnail(id);
+    const image = Buffer.from(imageEncoded, 'base64');
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': image.length
+    });
+    res.end(image);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default http;
