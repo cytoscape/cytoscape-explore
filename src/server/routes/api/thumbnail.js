57275d47b104c0f3e63d673b4b40d9ed0faef712
Mike Kucera <mikekucera@gmail.com>
2022-01-27 15:37:47 -0500

    Use default style.

--- src/server/routes/api/thumbnail.js
+++ src/server/routes/api/thumbnail.js
@@ -1,15 +1,20 @@
 import Express from 'express';
 import PouchDB  from 'pouchdb';
+import Cytoscape from 'cytoscape';
 import _ from 'lodash';
 import VizMapper from '../../../model/vizmapper';
-import Cytoscape from 'cytoscape';
-
+import { DEFAULT_NODE_STYLE, DEFAULT_EDGE_STYLE } from '../../../model/style';
 import { COUCHDB_PASSWORD, COUCHDB_URL, COUCHDB_USER, USE_COUCH_AUTH } from '../../env';
-
 
 // TODO Rewrite this to use 'import'
 const cytosnap = require('cytosnap');
 const snap = cytosnap();
+
+
+function objMap(obj, f) {
+  return Object.fromEntries(Object.entries(obj).map(([k,v]) => [k, f(v)]));
+}
+
 
 async function createThumbnail(id, width, height) {
   await snap.start();
@@ -19,12 +24,6 @@ async function createThumbnail(id, width, height) {
   console.log("PouchDB instance created!");
 
   const { elements, style } = await createCytoscapeElementsAndStyle(db, id);
-
-  console.log("Elements");
-  console.log(JSON.stringify(elements));
-
-  console.log("Style");
-  console.log(JSON.stringify(style));
 
   var options = {
     // cytoscape.js options
@@ -36,15 +35,14 @@ async function createThumbnail(id, width, height) {
     // image export options
     resolvesTo: 'base64', // output, one of 'base64uri' (default), 'base64', 'stream', or 'json' (export resultant node positions from layout)
     format: 'png', // 'png' or 'jpg'/'jpeg' (n/a if resolvesTo: 'json')
-    quality: 85, // quality of image if exporting jpg format, 0 (low) to 100 (high)
+    quality: 100, // quality of image if exporting jpg format, 0 (low) to 100 (high)
     background: 'transparent', // a css colour for the background (transparent by default)
     width, // the width of the image in pixels
     height // the height of the image in pixels
   };
   
   // TODO figure out how to use an image stream, shouldn't have to encode/decode base64
-  const image = await snap.shot(options);
-  return image;
+  return await snap.shot(options);
 }
 
 
@@ -73,7 +71,7 @@ async function createCytoscapeElementsAndStyle(db, id) {
     }
   }
   if(elements.length > 0){
-    cy.add(elements);
+    cy.add(elements); 
   }
 
   const vizmapper = new VizMapper(cy, null);
@@ -82,14 +80,19 @@ async function createCytoscapeElementsAndStyle(db, id) {
   const nodeStyleProps = Object.keys(_styles['node'] || {});
   const edgeStyleProps = Object.keys(_styles['edge'] || {});
 
-  const style = [];
+  const style = [{
+      selector: 'node',
+      style: objMap(DEFAULT_NODE_STYLE, val => val.stringValue)
+    },{
+      selector: 'edge',
+      style: objMap(DEFAULT_EDGE_STYLE, val => val.stringValue)
+  }];
 
   cy.elements().forEach(ele => {
     const styleProps = ele.isNode() ? nodeStyleProps : edgeStyleProps;
     const elementStyle = {};
     for(const prop of styleProps) {
-      const styleValue = vizmapper.calculate(ele, prop);
-      elementStyle[prop] = styleValue;
+      elementStyle[prop] = vizmapper.calculate(ele, prop);
     }
     style.push({
       selector: "#" + ele.id(),
